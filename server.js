778571edfea56c670fe6a63a9e15e222683e8a36
Alexander Efremov <efremov.sakha@gmail.com>
2023-01-25 22:26:11 +0700

    - Minor fixes and improvements; - Added typescript;

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/static`));
 
 app.get('*', (_, res) => {
   res.sendFile(`${__dirname}/dist/index.html`);
